69bcb4b4287ddfe45f89cf7d79e40d0e1bfea739
rudrasura <mpai1391@gmail.com>
2019-06-30 14:37:26 +0530

    Updates

---
+++ precache-manifest.a9f8eb1a50dbc91fa54a08ae7155b78e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66be97a19e235fa95a6c",
+    "url": "/me/static/js/runtime~main.66be97a1.js"
+  },
+  {
+    "revision": "4abc73d8cb966f8ccc2d",
+    "url": "/me/static/js/main.4abc73d8.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/me/static/js/2.9eee67cf.chunk.js"
+  },
+  {
+    "revision": "4abc73d8cb966f8ccc2d",
+    "url": "/me/static/css/main.4e53018a.chunk.css"
+  },
+  {
+    "revision": "377ad8e92571849682a36cab1f791017",
+    "url": "/me/index.html"
+  }
+];
